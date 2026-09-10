afbc35ec75bb052c3c83fd24b37f458d857182c0
Keiani <keiani1207@gmail.com>
2026-09-10 20:27:07 +0530

    update

--- decimal-binary.py
+++ decimal-binary.py
@@ -1,5 +1,9 @@
 Num = int(input("Enter a Number = "))
-if not Num.isdecimal:
-    print("Invalid input!!\nEnter the decimal number only..")
+if Num.isdecimal:
+    if Num == 0:
+      print("binary representation = 0")
+    else:
+       while Num > 0:
+          remainder = Num % 2
 else:
-    print
+    print("Invalid input!!\nEnter the decimal number only..")
