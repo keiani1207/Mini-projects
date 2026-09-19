c8dc60de6e30c9b2f71655215572df407918d295
Keiani <keiani1207@gmail.com>
2026-09-19 21:13:36 +0530

    update

--- 6.Reverse Digits Math Trick.py
+++ 6.Reverse Digits Math Trick.py
@@ -1,9 +1,11 @@
-num=(input("Enter 3 digit intger = "))
+num=(input("Enter digit 3 intger = "))
 print(num[::-1])
 
 #or
-num=(input("Enter 3 digit intger = "))
-num=num//100
+#without converting into strings
+num=int(input("Enter 3 digit intger = "))
+num0=num//100
 num1=num%100
 num2=num1//10
 num3=num1%10
+print(f"{num3}{num2}{num0}")
