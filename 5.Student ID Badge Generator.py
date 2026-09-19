6a92befb6561d6c93e3b60cc5455a78c533a717f
Keiani <keiani1207@gmail.com>
2026-09-19 19:02:18 +0530

    update

--- 5.Student ID Badge Generator.py
+++ 5.Student ID Badge Generator.py
@@ -2,4 +2,6 @@
 first_name = input("enter your first name = ")
 last_name = input("enter your last name = ")
 birth_year = int(input("enter your birth year = "))
-print(f"---------------------------")
+print(f"---------------------------\nSTUDENT BADGE\nNAME: {first_name} {last_name}\nYEAR OF BIRTH: {birth_year}")
+x=first_name[0] +last_name.upper() +"_" + birth_year
+print(f"USER ID: {x}")
